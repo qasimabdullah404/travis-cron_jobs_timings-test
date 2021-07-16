14eec555f7ad593046af621a5cf124371d09502a
Qasim Abdullah <qasimabdullah404@gmail.com>
2021-07-16 19:23:27 +0500

    ... TESTING IT OUT ...

--- index.js
+++ index.js
@@ -2,16 +2,20 @@ const cron = require('node-cron')
 const axios = require('axios')
 require('dotenv').config()
 
-axios.defaults.headers.common['Authorization'] = process.env.TRAVIS_AUTH_TOKEN
+TRAVIS_AUTH_TOKEN = process.env.TRAVIS_AUTH_TOKEN
+
+axios.defaults.headers.common['Authorization'] = `token ${TRAVIS_AUTH_TOKEN}`
 axios.defaults.headers.common['Travis-API-Version'] =  '3'
 axios.defaults.headers.post['Content-Type'] = 'application/json'
+axios.defaults.headers.post['Accept'] = 'application/json'
 
 const payload = {
     "request": {
-        "branch": "main"
+        "branch": "main",
+        "message": "This is an api request"
     }
 }
 
 cron.schedule("* * * * *", function () {
-    axios.post('https://api.travis-ci.com/repo/qasimabdullah404/travis-cron_jobs_timings-test/requests', payload)
+    axios.post('https://api.travis-ci.com/repo/qasimabdullah404%2Ftravis-cron_jobs_timings-test/requests', payload)
 })
